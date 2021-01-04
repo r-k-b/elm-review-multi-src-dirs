1a0858aff7dfc56e19ac5a56e62a5f28d1465bb4
Robert K. Bell <robert.k.bell@gmail.com>
2021-01-04 13:30:23 +1100

    prevent false-positive by sneaking in modules from other projects

--- example.sh
+++ example.sh
@@ -18,7 +18,11 @@ elm make src/MainC.elm --output=/dev/null
 popd
 
 echo
+echo "Because we have manually imported all modules, elm-review treats them as
+belonging to a single module, eliminating the false-positive while still
+detecting the true-positive of 'thisIsNeverUsed':"
+echo
 pushd b
-elm-review
+elm-review src/MainB.elm ../a/src/MainA.elm ../c/src/MainC.elm
 popd
 
