c9d881bb67cb91a5a41583f3e499c7942fb7e6b4
Robert K. Bell <robert.k.bell@gmail.com>
2021-01-04 12:40:47 +1100

    see what it's like when `nix run` is all that's needed
    e.g., it's a single command for an arbitrary Nix user to reproduce the
    errors I see when running ./example.sh

--- flake.nix
+++ flake.nix
@@ -7,12 +7,43 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
-        inherit (pkgs) mkShell elmPackages;
+        inherit (pkgs) mkShell elm2nix elmPackages stdenv graphviz nodejs;
+
+        elmTools = with elmPackages; [ elm elm-format elm-review ];
+
+        example = stdenv.mkDerivation {
+          name = "example";
+          buildInputs = elmTools ++ [ pkgs.bash ];
+          src = ./.;
+          buildPhase = ''
+            patchShebangs example.sh
+            mkdir elmhome
+            export ELM_HOME=./elmhome
+            ./example.sh
+          '';
+        };
+        exampleApp = utils.lib.mkApp { drv = example; };
+
+        svg = stdenv.mkDerivation {
+          name = "structure";
+          description = "a visual layout of the problematic structure";
+          buildInputs = [ graphviz ];
+          src = ./misc;
+          buildPhase = "dot -Tsvg -o structure.dot.svg structure.dot";
+          installPhase = "cp structure.dot.svg $out";
+        };
       in {
+        # `nix run`
+        apps.example = exampleApp;
+        defaultApp = exampleApp;
+
+        # `nix build`
+        packages.structure = svg;
+        defaultPackage = svg;
+
         # `nix develop`
         devShell = mkShell {
-          nativeBuildInputs = (with pkgs; [ nodejs ])
-            ++ (with elmPackages; [ elm elm-format elm-review ]);
+          nativeBuildInputs = ([ elm2nix graphviz nodejs ] ++ elmTools);
         };
       });
 }
